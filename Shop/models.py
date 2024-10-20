8964f2e468b6ecf5b5acae8194f4b1eb34661762
Aanchal-kumari-R <aanchalkumari096@gmail.com>
2024-10-20 10:09:28 +0530

    Adding Image and Changing Models

--- Shop/models.py
+++ Shop/models.py
@@ -8,6 +8,10 @@ class Product(models.Model):
     
     product_id = models.AutoField 
     product_name = models.CharField(max_length=50) 
+    category = models.CharField(max_length=50,default="") 
+    subCategory = models.CharField(max_length=50,default="")  
+    price = models.IntegerField(default=0)
     desc = models.CharField(max_length=300)  
-    pub_date = models.DateField()
+    pub_date = models.DateField() 
+    image = models.ImageField(upload_to="shop/Images",default="")
 
