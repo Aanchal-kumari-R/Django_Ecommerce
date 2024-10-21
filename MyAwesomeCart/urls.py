00b1fc6bf222b8e119ecda6381309298d97cb6d3
Aanchal-kumari-R <aanchalkumari096@gmail.com>
2024-10-21 12:50:41 +0530

    Showing Products on Homepage

--- MyAwesomeCart/urls.py
+++ MyAwesomeCart/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('Shop.urls')),
     path('blog/', include('Blog.urls')),
-] + static(settings.MEDIA_URL , document_root= settings.MEDIA_ROOT)
+]  + static(settings.MEDIA_URL , document_root= settings.MEDIA_ROOT)
