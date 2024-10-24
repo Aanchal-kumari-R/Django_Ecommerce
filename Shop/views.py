fcf0e42a411dc770ce976829964ce7ee357a896f
Aanchal-kumari-R <aanchalkumari096@gmail.com>
2024-10-24 12:21:59 +0530

    Adding Cart Items Count and Slider Logic

--- Shop/views.py
+++ Shop/views.py
@@ -5,10 +5,6 @@ from math import ceil
 # Create your views here. 
 
 def index(request):   
-    # products = Product.objects.all()  
-    # print(products)  
-    # n = len(products)
-    # nSlides = n//4 + ceil((n/4)-(n//4)) 
     allProds = [] 
     catprods = Product.objects.values('category','id') 
     cats = {item['category'] for item in catprods} 
@@ -17,10 +13,6 @@ def index(request):
         n = len(prod) 
         nSlides = n//4 + ceil((n/4)-(n//4)) 
         allProds.append([prod,range(1,nSlides),nSlides]) 
-
-    #params = {'no_of_slides':nSlides, 'range':range(1,nSlides),'product' : products} 
-    # allProds = [[products,range(1,nSlides),nSlides], 
-    #             [products,range(1,nSlides),nSlides]]  
     params = {'allProds':allProds}
     return render(request,"shop/index.html",params) 
 
