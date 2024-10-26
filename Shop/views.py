6020f698883ac5fba4b346c31ab31d261b4f197b
Aanchal-kumari-R <aanchalkumari096@gmail.com>
2024-10-26 12:24:15 +0530

    Creating Contact page & models

--- Shop/views.py
+++ Shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
-from .models import Product 
+from .models import Product ,Contact
 from math import ceil 
 # Create your views here. 
 
@@ -19,7 +19,14 @@ def index(request):
 def about(request): 
     return render(request,"shop/about.html")
 
-def contact(request): 
+def contact(request):  
+    if request.method == "POST": 
+      name = request.POST.get('name','')
+      email = request.POST.get('email','')
+      phone = request.POST.get('phone','')
+      desc = request.POST.get('desc','') 
+      contact = Contact(name=name,email =email,phone=phone,desc=desc) 
+      contact.save()
     return render(request,"shop/contact.html")
 
 def tracker(request): 
